e53cf1ff7e1752dceef91a9456693ba849c9405b
karabelnikov.s <karabelnikov.s@rwb.ru>
2025-12-09 20:19:37 +0300

    Добавил гейм стор для хранение текущей сессии игры

---
+++ Bomberman/Bomberman/Game/GameStateStore.swift
@@ -0,0 +1,56 @@
+//
+//  GameStateStore.swift
+//  Bomberman
+//
+//  Created by Karabelnikov Stepan on 09.12.2025.
+//
+
+import Foundation
+import Combine
+
+@MainActor
+final class GameStateStore: ObservableObject {
+
+    @Published var gameState: GameState?
+    @Published var playerID: String?
+    @Published var connectionState: WebSocketConnectionState = .disconnected
+
+    private let ws: WebSocketServiceProtocol
+    private let decoder: ServerEventDecoderProtocol
+    private var cancellables = Set<AnyCancellable>()
+
+    init(ws: WebSocketServiceProtocol, decoder: ServerEventDecoderProtocol) {
+        self.ws = ws
+        self.decoder = decoder
+        bind()
+    }
+
+    func bind() {
+        ws.messagePublisher
+            .sink { [weak self] text in
+                guard let event = self?.decoder.decode(text) else { return }
+                self?.handle(event)
+            }
+            .store(in: &cancellables)
+
+        ws.connectionStatePublisher
+            .assign(to: &$connectionState)
+    }
+
+    private func handle(_ event: ServerEvent) {
+        switch event {
+        case .assignID(let id):
+            playerID = id
+
+        case .gameState(let state):
+            gameState = state
+        }
+    }
+
+    func connect() { ws.connect() }
+    func disconnect() { ws.disconnect() }
+
+    func send(_ text: String) {
+        ws.send(text)
+    }
+}
